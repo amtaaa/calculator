6b9ddc8503172992bfa156d055b182798b869fb6
ameeraam <101507048+amtaaa@users.noreply.github.com>
2025-08-21 19:49:34 +0000

    Add all operations functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-screenDisplayedText = document.querySelector("#displayed-text");
+let screenDisplayedText = document.querySelector("#displayed-text");
 
-operandButtons = document.querySelectorAll("#operands div div");
+let operandButtons = document.querySelectorAll("#operands div div");
 
 let firstOperand = "";
 let secondOperand = "";
@@ -9,7 +9,7 @@ let operation = "";
 
 operandButtons.forEach(operandButton => {
     let content = operandButton.textContent;
-    if (!isNaN(parseInt(content))) {
+    if (!isNaN(parseInt(content)) || content == ".") {
         operandButton.addEventListener("click", function (e) {
             let newDisplayedNumber = "";
             if (operation == "") {
@@ -20,4 +20,40 @@ operandButtons.forEach(operandButton => {
             screenDisplayedText.textContent = newDisplayedNumber
         });
     }
+});
+
+let operationButtons = document.querySelectorAll("#operations div");
+operationButtons.forEach(operationButton => {
+    operationButton.addEventListener("click", function(e){
+        operation = operationButton.textContent;
+        secondOperand = "";
+    });
+});
+
+let equalButton = document.querySelector("#equal");
+equalButton.addEventListener("click", function(e){
+    let result = "";
+    if(operation == "+"){
+        result = String(Number(firstOperand) + Number(secondOperand));
+    }
+    if(operation == "-"){
+        result = String(Number(firstOperand) - Number(secondOperand));
+    }
+    if(operation == "x"){
+        result = String(Number(firstOperand) * Number(secondOperand));
+    }
+    if(operation == "/"){
+        result = String(Number(firstOperand) / Number(secondOperand));
+    }
+    firstOperand = result;
+    screenDisplayedText.textContent = result;
+    operation = "";
+    secondOperand = "";
+});
+
+document.querySelector("#AC").addEventListener("click", function(e){
+    firstOperand = "";
+    operation = "";
+    secondOperand = "";
+    screenDisplayedText.textContent = "0";
 });
